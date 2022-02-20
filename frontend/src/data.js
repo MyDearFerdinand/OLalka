081ab61fe706e13bc22e159ceb170c59fe66cbf2
Vladyslav Maidaniuk <vlad.maidanyuk95@gmail.com>
2022-02-20 22:10:52 +0100

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'Elisabeth doll',
+            category:'Shmatyanka',
+            image:'/images/p1.jpg',
+            price:59,
+            brand:'Olalka',
+            rating:5,
+            numReviews:10,
+            description: 'high quality interier doll',
+
+        },
+        {
+            _id:'2',
+            name:'Gloria doll',
+            category:'Shmatyanka',
+            image:'/images/p2.jpg',
+            price:59,
+            brand:'Olalka',
+            rating:4.5,
+            numReviews:34,
+            description: 'high quality interier doll',
+
+        },
+        {
+            _id:'3',
+            name:'Jess doll',
+            category:'Shmatyanka',
+            image:'/images/p3.jpg',
+            price:89,
+            brand:'Olalka',
+            rating:4.6,
+            numReviews:16,
+            description: 'high quality interier doll',
+
+        },
+        {
+            _id:'4',
+            name:'Kasia doll',
+            category:'Shmatyanka',
+            image:'/images/p4.jpg',
+            price:59,
+            brand:'Olalka',
+            rating:4,
+            numReviews:10,
+            description: 'high quality interier doll',
+
+        },
+        {
+            _id:'5',
+            name:'Busya doll',
+            category:'Shmatyanka',
+            image:'/images/p5.jpg',
+            price:69,
+            brand:'Olalka',
+            rating:4.8,
+            numReviews:100,
+            description: 'high quality interier doll',
+
+        },
+        {
+            _id:'6',
+            name:'Julya doll',
+            category:'Shmatyanka',
+            image:'/images/p6.jpg',
+            price:140,
+            brand:'Olalka',
+            rating:5,
+            numReviews:1000,
+            description: 'high quality interier doll',
+
+        },
+    ]
+}
+
+export default data;
